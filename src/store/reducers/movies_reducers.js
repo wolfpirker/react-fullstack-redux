b7c5c933b89714156d319e63336f13c277b2bdd3
wolfpirker <w_pirker@gmx.de>
2021-12-19 00:46:30 +0100

    creating a valid reducer

---
+++ src/store/reducers/movies_reducers.js
@@ -0,0 +1,8 @@
+export default function appReducer(state = {}, action) {
+    switch (action.type) {
+        case 'MOVIES_LIST':
+            return action.payload
+        default:
+            return state
+    }
+}
